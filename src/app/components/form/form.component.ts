2630215ddfb9a7feea510eef81a451e23801a7f5
Pepe Puertas Alonso <pepepuertasalonso4@gmail.com>
2025-02-22 12:10:21 +0100

    Implementación de la validación del formulario

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,10 +1,17 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { INew } from '../../interfaces/inew.interface';
 
 @Component({
   selector: 'app-form',
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css',
 })
@@ -18,17 +25,49 @@ export class FormComponent {
     date: new Date(),
   };
 
+  newForm: FormGroup;
+
+  constructor() {
+    this.newForm = new FormGroup(
+      {
+        title: new FormControl('', [Validators.required]),
+        image: new FormControl('', [Validators.required]),
+        body: new FormControl('', [Validators.required]),
+        date: new FormControl(null, [
+          Validators.required,
+          this.dateLessThanTodayValidator,
+        ]),
+      },
+      []
+    );
+  }
   getData() {
-    this.sendNew.emit(this.dataForm);
-    this.resetForm();
+    this.sendNew.emit(this.newForm.value);
+    this.newForm.reset();
   }
 
-  resetForm() {
-    this.dataForm = {
-      title: '',
-      image: '',
-      body: '',
-      date: new Date(),
-    };
+  checkControl(controlName: string, errorName: string) {
+    return (
+      this.newForm.get(controlName)?.hasError(errorName) &&
+      this.newForm.get(controlName)?.touched
+    );
+  }
+
+  /*
+   * Comprueba que la fecha de publicación no sea posterior al día actual
+   */
+  dateLessThanTodayValidator(controlName: AbstractControl): any {
+    if (!controlName.value) {
+      return null;
+    }
+    const inputDate = new Date(controlName.value);
+    const today = new Date();
+
+    if (inputDate >= today) {
+      return {
+        dateLessThanTodayValidator: 'La fecha debe de ser anterior a hoy.',
+      };
+    }
+    return null;
   }
 }
